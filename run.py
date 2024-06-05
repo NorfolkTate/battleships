3b8a430f772963bd9bb81c228d8810f92dcb5f83
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-06-05 09:55:21 +0000

    add function to get player name

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from random import randint
 
+def name_and_rules():
+    """"
+    function to get user's name and display the rules
+    of the game
+    """
+    player_name = input("enter your name: \n")
+    print(f"Let's play battleships, {player_name}")
 
+name_and_rules()
